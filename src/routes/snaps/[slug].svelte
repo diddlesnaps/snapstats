9d1bf867abcab3ee80c1276c54a2b257f25b5ddf
Daniel Llewellyn <diddledan@ubuntu.com>
2020-09-05 17:20:49 +0100

    replace template literals with svelte interpolated strings
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- src/routes/snaps/[slug].svelte
+++ src/routes/snaps/[slug].svelte
@@ -111,7 +111,7 @@
                     video.type = 'vimeo'
                 }
                 if (video.url.match(/asciinema/)) {
-                    video.url = video.url = video.url + '.js'
+                    video.url = video.url + '.js'
                     video.type = 'asciinema'
                 }
                 video.url = video.url.replace('http://', 'https://')
@@ -338,25 +338,25 @@
             <picture>
                 <source
                     media="(min-width: 896px)"
-                    srcset="{`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_896/${result.data.snapByName.media.find(item => item.type === 'banner').url}`},
-                        {`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_1792/${result.data.snapByName.media.find(item => item.type === 'banner').url}`} 2x" />
+                    srcset="https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_896/{result.data.snapByName.media.find(item => item.type === 'banner').url},
+                        https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_1792/{result.data.snapByName.media.find(item => item.type === 'banner').url} 2x" />
                 <source
                     media="(max-width: 720px)"
-                    srcset="{`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_720/${result.data.snapByName.media.find(item => item.type === 'banner').url}`},
-                        {`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_1440/${result.data.snapByName.media.find(item => item.type === 'banner').url}`} 2x" />
+                    srcset="https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_720/{result.data.snapByName.media.find(item => item.type === 'banner').url},
+                        https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_1440/{result.data.snapByName.media.find(item => item.type === 'banner').url} 2x" />
                 <source
                     media="(max-width: 640px)"
-                    srcset="{`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_640/${result.data.snapByName.media.find(item => item.type === 'banner').url}`},
-                        {`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_1280/${result.data.snapByName.media.find(item => item.type === 'banner').url}`} 2x" />
+                    srcset="https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_640/{result.data.snapByName.media.find(item => item.type === 'banner').url},
+                        https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_1280/{result.data.snapByName.media.find(item => item.type === 'banner').url} 2x" />
                 <source
                     media="(max-width: 480px)"
-                    srcset="{`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_480/${result.data.snapByName.media.find(item => item.type === 'banner').url}`},
-                        {`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_960/${result.data.snapByName.media.find(item => item.type === 'banner').url}`} 2x" />
+                    srcset="https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_480/{result.data.snapByName.media.find(item => item.type === 'banner').url},
+                        https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_960/{result.data.snapByName.media.find(item => item.type === 'banner').url} 2x" />
                 <img class='bannerImage' loading="lazy"
-                    width={result.data.snapByName.media.find(item => item.type === 'banner').width || 1920}
-                    height={result.data.snapByName.media.find(item => item.type === 'banner').height || 640}
-                    src={`https://res.cloudinary.com/canonical/image/fetch/${result.data.snapByName.media.find(item => item.type === 'banner').url}`}
-                    alt={`${result.data.snapByName.title || result.data.snapByName.package_name} banner`} />
+                    width="{result.data.snapByName.media.find(item => item.type === 'banner').width || 1920}"
+                    height="{result.data.snapByName.media.find(item => item.type === 'banner').height || 640}"
+                    src="https://res.cloudinary.com/canonical/image/fetch/{result.data.snapByName.media.find(item => item.type === 'banner').url}"
+                    alt="{result.data.snapByName.title || result.data.snapByName.package_name} banner" />
             </picture>
         {/if}
         <div class='banner'>
@@ -364,11 +364,11 @@
                 <p class='icon'>
                     <picture>
                         <source
-                            srcset="{`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_128/${result.data.snapByName.icon_url}`},
-                                {`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_256/${result.data.snapByName.icon_url}`} 2x" />
+                            srcset="https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_128/{result.data.snapByName.icon_url},
+                                https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_256/{result.data.snapByName.icon_url} 2x" />
                         <img height="128" width="128" loading="lazy"
-                            src={`https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_128/${result.data.snapByName.icon_url}`}
-                            alt={`Icon for ${result.data.snapByName.title || result.data.snapByName.package_name}`} />
+                            src="https://res.cloudinary.com/canonical/image/fetch/q_auto,f_auto,w_128/{result.data.snapByName.icon_url}"
+                            alt="Icon for {result.data.snapByName.title || result.data.snapByName.package_name}" />
                     </picture>
                 </p>
             {/if}
@@ -469,12 +469,12 @@
                     {#if video && 'url' in video && 'type' in video}
                         {#if video.type === 'youtube'}
                             <iframe id="ytplayer" type="text/html" width="818" height="460" title="Youtube player"
-                                src={`${video.url}?autoplay=1&mute=1&modestbranding=1&rel=0`} frameborder="0"></iframe>
+                                src="{video.url}?autoplay=1&mute=1&modestbranding=1&rel=0" frameborder="0"></iframe>
                         {:else if video.type === 'vimeo'}
                             <iframe id="vimeoplayer" width="818" height="460" frameborder="0" title="Vimeo player"
-                                src={`${video.url}?title=0&byline=0&portrait=0&transparent=0`}></iframe>
+                                src="{video.url}?title=0&byline=0&portrait=0&transparent=0"></iframe>
                         {:else if video.type === 'asciinema'}
-                            <script src={video.url} id="asciicast" async data-autoplay="1" data-preload="0"></script>
+                            <script src="{video.url}" id="asciicast" async data-autoplay="1" data-preload="0"></script>
                         {/if}
                     {/if}
                     {#each result.data.snapByName.media.filter(item => item.type === 'screenshot') as screenshot}
