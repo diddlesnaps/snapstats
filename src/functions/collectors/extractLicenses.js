15ff8926a4afe789e3e89f739f4cc1426450901b
Daniel Llewellyn <diddledan@ubuntu.com>
2021-04-16 14:18:03 +0100

    Update collectors/extractLicenses.js
    * Improve memory and time requirements
    
    Signed-off-by: Daniel Llewellyn <diddledan@ubuntu.com>

--- src/functions/collectors/extractLicenses.js
+++ src/functions/collectors/extractLicenses.js
@@ -3,8 +3,20 @@
 import { connectMongoose } from '../../mongodb.js';
 import {SnapsModel} from '../../models/Snaps.js';
 import {LicensesModel} from '../../models/License.js';
-import { sort, extractCombinedLicenseCounts, getCounts, mapCounts } from '../../statsHelpers.js';
+import { sort, extractCombinedLicenseCounts, mapCounts } from '../../statsHelpers.js';
+import { promisify } from '../../promisify.js';
 
+const getCounts = items => {
+    const item_names = new Set(items.map(item => item.name))
+    /** @type {{[key: string]: number}} */
+    const item_counts = {};
+    for (const name of item_names) {
+        const this_item = items.find(el => el.name === name)
+        item_counts[name] = this_item.count;
+    }
+
+    return item_counts
+}
 const collector = (isDaily = false) => async (context) => {
     const addDate = (dateKey = null) => (data) => {
         const key = dateKey || 'date';
@@ -18,8 +30,19 @@ const collector = (isDaily = false) => async (context) => {
 
     connectMongoose();
 
-    const snaps          = await SnapsModel.find().select({license:1, _id:0})
-    const license_counts = extractCombinedLicenseCounts(getCounts('license', snaps));
+    const snap_licenses  = await promisify(SnapsModel.aggregate([
+        { $group: {
+            '_id': '$license',
+            'count': { $sum: 1 },
+        } },
+        { $project: {
+            'name': '$_id',
+            'count': '$count',
+            '_id': 0,
+        } },
+    ]))
+
+    const license_counts = extractCombinedLicenseCounts(getCounts(snap_licenses))
     const licenses       = sort(Object.keys(license_counts).map(mapCounts(license_counts)));
 
     await LicensesModel.insertMany(
