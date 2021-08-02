70401f3e6829dfc6f7eaf5e9ad59706acf2f0b31
Daniel Llewellyn <diddledan@ubuntu.com>
2021-08-02 21:41:33 +0100

    Update functions schedules
    Signed-off-by: Daniel Llewellyn <diddledan@ubuntu.com>

--- src/functions/collectors/index.js
+++ src/functions/collectors/index.js
@@ -5,21 +5,21 @@ import * as functions from 'firebase-functions';
 export const hourlyStats = functions.runWith({
     timeoutSeconds: 540,
     memory: '512MB'
-}).pubsub.schedule('every 1 hours').onRun(async (context) => (await import('./collectStats')).hourly(context));
+}).pubsub.schedule('every 1 hour').onRun(async (context) => (await import('./collectStats')).hourly(context));
 export const dailyStats = functions.runWith({
     timeoutSeconds: 540,
     memory: '512MB'
-}).pubsub.schedule('every day').onRun(async (context) => (await import('./collectStats')).daily(context));
+}).pubsub.schedule('every 1 day').onRun(async (context) => (await import('./collectStats')).daily(context));
 export const dailyLicenses = functions.runWith({
     timeoutSeconds: 30,
     memory: '256MB',
-}).pubsub.schedule('every day').onRun(async (context) => (await import('./extractLicenses')).daily(context));
+}).pubsub.schedule('every 1 day').onRun(async (context) => (await import('./extractLicenses')).daily(context));
 // export const dailyStats = functions.pubsub.schedule('every 24 hours').onRun(collectStats(true));
 export const dailyRatings = functions.runWith({
     timeoutSeconds: 30,
     memory: '256MB',
-}).pubsub.schedule('every day').onRun(async (context) => (await import('./collectRatings')).default(context));
+}).pubsub.schedule('every 1 day').onRun(async (context) => (await import('./collectRatings')).default(context));
 export const dailyThinStats = functions.runWith({
     timeoutSeconds: 300,
     memory: '256MB',
-}).pubsub.schedule('every day').onRun(async (context) => (await import('./thinStats')).default(context));
+}).pubsub.schedule('every 1 day').onRun(async (context) => (await import('./thinStats')).default(context));
