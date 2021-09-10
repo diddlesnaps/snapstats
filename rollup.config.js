7649f2a2a715acc79c37d7c3269a3aa7ff54cb7c
Dani Llewellyn <diddledani@ubuntu.com>
2021-09-10 15:40:33 +0100

    Drop unused module import in rollup config
    Signed-off-by: Dani Llewellyn <diddledani@ubuntu.com>

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import typescript from '@rollup/plugin-typescript';
 import autoPreprocess from 'svelte-preprocess';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
