4c95ead0614e345710b7d40f72a5c903056918e2
Dani Llewellyn <diddledani@ubuntu.com>
2021-09-12 15:59:41 +0100

    Add width and height to donate button image
    Signed-off-by: Dani Llewellyn <diddledani@ubuntu.com>

--- src/components/DonateBtn.svelte
+++ src/components/DonateBtn.svelte
@@ -13,5 +13,5 @@
 </style>
 
 <p class='donate'>
-    Enjoying this site? Please <a href="https://liberapay.com/diddledani/donate" on:click={logConversion}><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg"/></a> to help keep the Snapstats.org lights on.
+    Enjoying this site? Please <a href="https://liberapay.com/diddledani/donate" on:click={logConversion}><img width="83" height="30" alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg"/></a> to help keep the Snapstats.org lights on.
 </p>
